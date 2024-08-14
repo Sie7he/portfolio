88872d0c7d6a655cac91598caf1c0d2f3dd6c602
Diego Romero <diegoescurraromero@gmail.com>
2024-08-13 22:02:45 -0400

    fix nav

--- src/components/shareds/navbar/Navbar.jsx
+++ src/components/shareds/navbar/Navbar.jsx
@@ -3,14 +3,9 @@ import './navbar.css'
 
 
 export const Navbar = () => {
-  let lastScrollTop = 0;
-  const [activeIndex, setActiveIndex] = useState(0);
 
   const [active, setActive] = useState('inicio');
 
-  const handleNavClick = (navItem) => {
-    setActive(navItem);
-  };
 
   useEffect(() => {
     const sections = document.querySelectorAll('section'); // Asegúrate de que tus secciones tengan la etiqueta <section>
@@ -41,13 +36,12 @@ export const Navbar = () => {
   const nav = [{id: 1, text:'inicio'}, {id: 2, text:'portafolio'}, {id: 3, text: 'habilidades'}, {id: 4, text: 'contacto'}]
 
   return (
-    <header className='flex justify-center relative' >
-      <nav id='navbar' >
+    <header className='flex justify-center relative'>
+      <nav id='navbar' className='visible'>
 
         {nav.map( item => (
         <a
         className={`nav-item ${active === item.text ? 'active' : ''}`}
-        onClick={() => handleNavClick(item.text)}
         href={`/#${item.text}`}
         key={item.id}
         >
